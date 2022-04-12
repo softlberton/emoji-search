7df35140acb5510f0693625615ae0d9125a9364d
ValKiriann <ValKiriann@users.noreply.github.com>
2022-04-12 13:56:09 +0000

    deploy: 5522842b8341b7fc7f9ee8f9f07b02ffd4034f9e

---
+++ precache-manifest.c16293911cc2296f81a8b2e35d020866.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c144c4fa5cb012fb88a437641ba0f1",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "cf06f4c090f6321192f9",
+    "url": "/emoji-search/static/css/2.90da6880.chunk.css"
+  },
+  {
+    "revision": "956b2e7050e90f2026af",
+    "url": "/emoji-search/static/css/main.65605343.chunk.css"
+  },
+  {
+    "revision": "cf06f4c090f6321192f9",
+    "url": "/emoji-search/static/js/2.9f764114.chunk.js"
+  },
+  {
+    "revision": "2ce1509b9f5d601b23dc0321a205137e",
+    "url": "/emoji-search/static/js/2.9f764114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "956b2e7050e90f2026af",
+    "url": "/emoji-search/static/js/main.1b9e6b49.chunk.js"
+  },
+  {
+    "revision": "d44d1531576180c634c7",
+    "url": "/emoji-search/static/js/runtime-main.d8cd5ee4.js"
+  }
+]);

008f7fdc7fcf96cefc4bb88fd285f2084b793878
softlberton <SOFT.lberton@iberia.es>
2022-06-07 16:29:47 +0200

    feat: Update Header.css

--- src/Header.js
+++ src/Header.js
@@ -6,14 +6,14 @@ export default class Header extends PureComponent {
     return (
       <header className="component-header">
         <img
-          src="//cdn.jsdelivr.net/emojione/assets/png/1f638.png"
+          src="//cdn.jsdelivr.net/emojione/assets/png/1f644.png"
           width="32"
           height="32"
           alt=""
         />
-        Emoji Search Demo
+        Emoji Search SAFe DevSecOps Iberia
         <img
-          src="//cdn.jsdelivr.net/emojione/assets/png/1f63a.png"
+          src="//cdn.jsdelivr.net/emojione/assets/png/1f644.png"
           width="32"
           height="32"
           alt=""
